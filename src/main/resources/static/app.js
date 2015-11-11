6b830c1494a714f45f92c0bf6d53684f0e6ea322
gallanthunter <gallanthunter@163.com>
2015-11-11 23:25:45 +0800

    modified:   src/main/resources/static/app.js new file:   src/main/resources/static/controllers/main.ctrl.js new file:   src/main/resources/static/images/asset.png modified:   src/main/resources/static/index.html new file:   src/main/resources/static/views/assetList.html modified:   src/main/resources/static/views/footer.html modified:   src/main/resources/static/views/header.html deleted:    .bowerrc deleted:    package.json deleted:    src/main/java/pers/zhangzhijun/amp/config/MongoDBConfiguration.java

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,3 +1,23 @@
 /**
  * Created by ZhangZhijun on 2015/8/30.
  */
+angular.module('app',[
+    'ngRoute'
+])
+    .config(function($routeProvider){
+        $routeProvider
+        .when('/',{
+            templateUrl:'',
+            controller:''
+        })
+        .when('/login',{
+
+        })
+        .when('/asset/',{
+
+        })
+        .otherwise({
+
+        })
+    })
+;
---
+++ src/main/resources/static/controllers/main.ctrl.js
@@ -0,0 +1,9 @@
+/**
+ * Function:
+ * Author:ZhangZhijun
+ * Date: 2015/11/11
+ */
+angular.module('app').controller('MainController', function(){
+    var vm = this;
+    vm.title = 'asset management platfrom!'
+});
