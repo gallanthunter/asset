1873c7f54a12380a6ef95ffcd29e7170acfe5e9f
gallanthunter <gallanthunter@163.com>
2015-12-16 23:47:03 +0800

    add angular-chart.js

--- src/main/resources/static/controllers/main.ctrl.js
+++ src/main/resources/static/controllers/main.ctrl.js
@@ -3,7 +3,7 @@
  * Author:ZhangZhijun
  * Date: 2015/11/11
  */
-angular.module('app').controller('MainController', function(){
+angular.module('app',['chart.js']).controller('MainController', function(){
     var vm = this;
     vm.title = 'asset management platfrom!'
 });
